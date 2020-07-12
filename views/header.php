a756f1539bc2044d70a1cc89ba07d04285525de5
william patiño <wpatino1989@gmail.com>
2020-07-12 14:24:01 -0500

    ajuste interfaz de la aplicacion.

--- views/header.php
+++ views/header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Captcha</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/estilos.css">
+    <link rel="stylesheet" href="../public/css/estilos.css">
+    <link rel="stylesheet" href="../public/css/table.css">
 
 </head>
 

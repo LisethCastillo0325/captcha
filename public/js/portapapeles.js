b031efea7f8ee072201cb8b3632310e8e18f2240
william patiño <wpatino1989@gmail.com>
2020-07-17 19:47:16 -0500

    Se creo el boton de copiar url

---
+++ public/js/portapapeles.js
@@ -0,0 +1,32 @@
+var clipboard = new Clipboard('.portapapeles');
+var divAlerta = document.getElementById('alerta');
+
+
+
+clipboard.on('success', function(e) {
+    exito();
+    mostrarAlerta();
+});
+
+clipboard.on('error', function(e) {
+	// Algo no salio como debia
+    fracaso();
+    mostrarAlerta();
+});
+
+
+
+function exito() {
+    divAlerta.innerText = '¡¡Código copiado al portapapeles!!';
+    divAlerta.classList.add('alert-success');
+}
+
+function fracaso() {
+    divAlerta.innerText = '¡¡Ha fallado el copiado al portapapeles!!';
+    divAlerta.classList.add('alert-warning');
+}
+function mostrarAlerta() {
+    divAlerta.classList.remove('invisible');
+    divAlerta.classList.add('visible');
+    setTimeout(ocultarAlerta, 1500);
+}
